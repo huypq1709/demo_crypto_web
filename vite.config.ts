5c391a5403486bf37c97a730433e82725d426e9c
huypq179 <huypq179@gmail.com>
2025-10-05 22:28:13 +0700

    Build project for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Thêm dòng này vào
+  base: '/demo_crypto_web/' 
 })
